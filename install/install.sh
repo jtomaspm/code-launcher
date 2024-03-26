b1e60ec3519b946a4eb565488e821c497c77564e
João Machado <joaotomachado@proton.me>
2024-03-26 20:17:04 +0000

    added fzf

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,13 @@
 
 greeting() {
     echo ""
-    echo "Installing code launcher..."
+    echo "Installing code-launcher..."
     echo "Make sure the following dependencies are installed:"
-    echo "    - Git CLI"
+    echo "    - git-cli"
+    echo ""
+    echo "Installing dependencies: "
+    echo "    - fzf"
+    echo "    - code-launcher"
     echo ""
     echo "Continue?"
     read -p "[Y/n] " opt
@@ -15,14 +19,32 @@ greeting() {
     fi
 }
 
+check_installed() {
+    local program_name="$1"
+    if command -v "$program_name" &> /dev/null; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 install_fzf() {
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 }
 
+install_deps() {
+    check_installed "fzf"
+    if [[ $? -eq 0 ]]; then
+        install_fzf
+    else
+        echo "fzf already installed, skipping..."
+    fi
+}
+
 main() {
     greeting
-    install_fzf
+    install_deps
 } 
 
 main
